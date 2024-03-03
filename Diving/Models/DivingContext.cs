b8dae7fc89c7672d1a3037a6c5d25be10672749d
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-03-03 19:27:05 +0100

    Initial migration

--- Diving/Models/DivingContext.cs
+++ Diving/Models/DivingContext.cs
@@ -9,7 +9,11 @@ public class DivingContext : DbContext
         : base(options)
     {
     }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=C:\\DB\\diving.db");
 
+    
     public DbSet<Client> Clients { get; set; } = null!;
     public DbSet<Course> Courses { get; set; } = null!;
     public DbSet<Instructor> Instructors { get; set; } = null!;
--- Diving/Program.cs
+++ Diving/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DivingContext>(opt =>
-    opt.UseInMemoryDatabase("DivingList"));
+    opt.UseSqlite("Data Source=C:\\DB\\diving.db"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
